76c6eeded28c460b3fae930f918f9585d9e93839
Saddam Hussain <sishack8@gmail.com>
2020-08-22 22:09:22 +0530

    Latest

---
+++ resources/views/admin/view_notice.blade.php
@@ -0,0 +1,30 @@
+
+@extends('admin.template.admin_master')
+
+@section('content')
+
+<!-- page content -->
+<div class="right_col" role="main">
+        <div class="row">
+                {{-- <div class="col-md-2"></div> --}}
+                <div class="col-md-12" style="margin-top:50px;">
+                    <div class="x_panel">
+    
+                        <div class="x_title">
+                            <h2>{{$notice->title}}</h2>
+                            <div class="clearfix"></div>
+                        </div>
+                    <div>
+                    </div>
+                        <div>
+                            <div class="x_content">
+                                <p>{{$notice->description}}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {{-- <div class="col-md-2"></div> --}}
+        </div>
+</div>
+<!-- /page content -->
+@endsection
