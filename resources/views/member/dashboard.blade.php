4b5b37fcb6e6d229d3a4076abdee00cddaddaef6
Saddam Hussain <sishack8@gmail.com>
2020-08-26 12:57:50 +0530

    Update

--- resources/views/member/dashboard.blade.php
+++ resources/views/member/dashboard.blade.php
@@ -76,7 +76,7 @@
                 <h3>Available Fund</h3>
               </div>
             </div>
-            <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            {{-- <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
               <div class="tile-stats">
                 <div class="icon"><i class="fa fa-sort-amount-desc"></i></div>
                 <div class="count">0</div>
@@ -89,7 +89,7 @@
                 <div class="count">0</div>
                 <h3>Right Active</h3>
               </div>
-            </div>
+            </div> --}}
           </div>
           {{-- <div class="row top_tiles">
             <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
